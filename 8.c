f4bb04b712af7e6ef102cd22fe998b1fecbc1f49
EAVN <evelascon@unal.edu.co>
2022-10-27 15:17:57 -0500

    solución al punto 8 del parcial 2

---
+++ 8.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main() {
+    int a, b;
+    // a <= b
+    scanf("%d", &a);
+    scanf("%d", &b);
+    // primos entre a y b
+    // primos mayores que 9 ? (cuántos digitos tiene? ==> obtener digitos ==> su suma es un primo ? imprimir número original)
+    for (int i = a; i <= b; i++) {
+        bool primo = true;
+        if (i < 2) {
+            primo = false;
+        } else {
+            for (int j = 2; j < i && primo; j++) {
+                if (i%j == 0 && i != j) {
+                    primo = false;
+                }
+            }
+            if (primo == true) {
+                if (i > 9) {
+                    int numDigits = 0;
+                    while (i / (int) pow(10, numDigits) >= 1) {
+                        numDigits++;
+                    }
+                    int sum = 0;
+                    for (int k = 0; k <= numDigits; k++) {
+                        int paso = i / (int) pow(10, k);
+                        int resta = 0;
+                        if (paso > 9) {
+                            resta = i / (int) pow(10, k + 1);
+                        }
+                        int result = paso - resta * 10;
+                        sum += result;
+                    }
+                    if (sum < 2) {
+                        primo = false;
+                    } else {
+                        for (int l = 2; l < sum; l++) {
+                            if (sum%l == 0 && sum != l) {
+                                primo = false;
+                            }
+                        }
+                    }
+                    if (primo == true) {
+                        printf("%d\n", i);
+                    }
+                } else {
+                    printf("%d\n", i);
+                }
+            }
+        }
+    }
+    return 0;
+}
